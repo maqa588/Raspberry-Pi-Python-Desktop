1fe48e8d176f29573cdeb104833ed700894f7b83
Spencer Maqa <maqa588@163.com>
2025-09-15 16:47:36 +0800

    Resize Window Height

--- desktop.py
+++ desktop.py
@@ -11,7 +11,7 @@ from system.CreatePlaceholderIcon import create_placeholder_icon
 # --- 配置 ---
 CONFIG_FILE = 'desktop_layout.json'
 WINDOW_WIDTH = 480
-WINDOW_HEIGHT = 320
+WINDOW_HEIGHT = 290
 # 画布的虚拟大小，大于窗口尺寸以实现滚动效果
 CANVAS_WIDTH = 800
 CANVAS_HEIGHT = 600
