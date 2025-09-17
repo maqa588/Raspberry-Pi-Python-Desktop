8603160f8e0b3ec8230bbe12837e159e282d41d7
Spencer Maqa <maqa588@163.com>
2025-09-17 22:56:41 +0800

    fix bug

--- software/camera_rpi.py
+++ software/camera_rpi.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import signal
 import datetime
 import subprocess
@@ -7,6 +8,12 @@ from tkinter import messagebox
 from picamera2 import Picamera2
 from PIL import Image, ImageTk
 import numpy as np
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, '..', '..'))
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
 from system.button.about import show_system_about, show_developer_about
 from system.config import WINDOW_HEIGHT, WINDOW_WIDTH
 
