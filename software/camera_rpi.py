8f2bc9590231ec0656019dfba0622af2c35674eb
Spencer Maqa <maqa588@163.com>
2025-09-17 23:04:35 +0800

    fix bug

--- software/camera_rpi.py
+++ software/camera_rpi.py
@@ -1,4 +1,10 @@
 import os
+import sys
+
+current_file_path = os.path.abspath(__file__)
+project_root = os.path.dirname(os.path.dirname(current_file_path))
+sys.path.insert(0, project_root)
+
 import signal
 import datetime
 import subprocess
